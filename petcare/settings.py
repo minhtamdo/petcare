150041c0ec82c302956731153e6130b3551520f9
minhchau-creator <84848387+minhchau-creator@users.noreply.github.com>
2025-06-12 11:48:43 +0700

    lam on duoc di

--- petcare/settings.py
+++ petcare/settings.py
@@ -86,6 +86,7 @@ DATABASES = {
     }
 }
 
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -124,5 +125,6 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
