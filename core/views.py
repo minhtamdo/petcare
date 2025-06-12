bc4058c30c6057d11097c7cb8d6068ad10c68653
minhchau-creator <84848387+minhchau-creator@users.noreply.github.com>
2025-06-12 11:44:52 +0700

    lam on duoc di

--- core/views.py
+++ core/views.py
@@ -1,9 +1,2 @@
 from django.shortcuts import render
-from .models import MedicalHistory, Appointment, User 
-
-
-from django.db import connection
-
-from django.db import connection
-from django.shortcuts import render
 
